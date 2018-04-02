1ae6a8150eea4069238c127d8d324fbce4fb195a
Byrne Gabrielle Libanan <byrne.code@gmail.com>
2018-04-02 08:07:43 +0800

    Updates to some variable declarations.

--- js/phsm-computations.js
+++ js/phsm-computations.js
@@ -3,7 +3,7 @@ let PHSM = (function () {
 	// ************************************************************************
 	// PRIVATE VARIABLES
 	// ************************************************************************
-	const charges = {
+	const CHARGES = {
 		commission: 0.25, // % of gross amount
 		commissionMinimum: 20, // pesos minimum commission of broker
 		vat: 12, // % of commission
@@ -13,98 +13,98 @@ let PHSM = (function () {
 	};
 
 	// Board Lot Table (minimum allowed shares at certain price range)
-	const boardLotTable = [
+	const BOARD_LOT_TABLE = [
 		{
 			from: 0.0001,
 			to: 0.0099,
 			tick: 0.0001,
 			size: 1000000
-        },
+		},
 		{
 			from: 0.0100,
 			to: 0.0490,
 			tick: 0.0010,
 			size: 100000
-        },
+		},
 		{
 			from: 0.0500,
 			to: 0.2490,
 			tick: 0.0010,
 			size: 10000
-        },
+		},
 		{
 			from: 0.2500,
 			to: 0.4950,
 			tick: 0.0050,
 			size: 10000
-        },
+		},
 		{
 			from: 0.5000,
 			to: 4.9900,
 			tick: 0.0100,
 			size: 1000
-        },
+		},
 		{
 			from: 5.0000,
 			to: 9.9900,
 			tick: 0.0100,
 			size: 100
-        },
+		},
 		{
 			from: 10.0000,
 			to: 19.9800,
 			tick: 0.0200,
 			size: 100
-        },
+		},
 		{
 			from: 20.0000,
 			to: 49.9500,
 			tick: 0.0500,
 			size: 100
-        },
+		},
 		{
 			from: 50.0000,
 			to: 99.9500,
 			tick: 0.0500,
 			size: 10
-        },
+		},
 		{
 			from: 100.0000,
 			to: 199.9000,
 			tick: 0.1000,
 			size: 10
-        },
+		},
 		{
 			from: 200.0000,
 			to: 499.8000,
 			tick: 0.2000,
 			size: 10
-        },
+		},
 		{
 			from: 500.0000,
 			to: 999.5000,
 			tick: 0.5000,
 			size: 10
-        },
+		},
 		{
 			from: 1000.0000,
 			to: 1999.0000,
 			tick: 1.0000,
 			size: 5
-        },
+		},
 		{
 			from: 2000.0000,
 			to: 4998.0000,
 			tick: 2.0000,
 			size: 5
-        },
+		},
 		{
 			from: 5000.0000,
 			to: 9999.0000,
 			tick: 5.0000,
 			size: 5
-        }
-    ];
+		}
+  ];
 
 	// ========================================================================
 	// private function to get Board Lot size
@@ -114,11 +114,11 @@ let PHSM = (function () {
 	const getBoardLot = function (price) {
 		let result;
 
-		for (let i = 0; i < boardLotTable.length; i++) {
-			const from = boardLotTable[i].from, // price range (from)
-				to = boardLotTable[i].to, // price range (to)
-				tick = boardLotTable[i].tick, // tick size (fluctuation)
-				size = boardLotTable[i].size; // lot size (minimum shares)
+		for (let i = 0; i < BOARD_LOT_TABLE.length; i++) {
+			const from = BOARD_LOT_TABLE[i].from; // price range (from)
+			const	to = BOARD_LOT_TABLE[i].to; // price range (to)
+			const	tick = BOARD_LOT_TABLE[i].tick; // tick size (fluctuation)
+			const	size = BOARD_LOT_TABLE[i].size; // lot size (minimum shares)
 
 			// check for price range
 			if (price >= from && price <= to) {
@@ -141,8 +141,8 @@ let PHSM = (function () {
 	// return: int/float - computed commission
 	// ========================================================================
 	const computeCommission = function (grossAmount) {
-		const commission = charges.commission,
-			commissionMinimum = charges.commissionMinimum;
+		const commission = CHARGES.commission;
+		const	commissionMinimum = CHARGES.commissionMinimum;
 		// get (commission)% of gross amount
 		let computedCommission = PHSM.whatIsPercentOf(commission, grossAmount);
 
@@ -161,7 +161,7 @@ let PHSM = (function () {
 	// return: int/float - computed vat
 	// ========================================================================
 	const computeVat = function (commission) {
-		const vat = charges.vat;
+		const vat = CHARGES.vat;
 		// get (vat)% of commission
 		let computedVat = PHSM.whatIsPercentOf(vat, commission);
 
@@ -176,7 +176,7 @@ let PHSM = (function () {
 	// return: int/float - computed pse trans fee
 	// ========================================================================
 	const computePseTransFee = function (grossAmount) {
-		const pseTransFee = charges.pseTransFee;
+		const pseTransFee = CHARGES.pseTransFee;
 		// get (pse trans fee)% of gross amount
 		let computedPseTransFee = PHSM.whatIsPercentOf(pseTransFee, grossAmount);
 
@@ -191,7 +191,7 @@ let PHSM = (function () {
 	// return: int/float - computed sccp fee
 	// ========================================================================
 	const computeSccp = function (grossAmount) {
-		const sccp = charges.sccp;
+		const sccp = CHARGES.sccp;
 		// get (sccp fee)% of gross amount
 		let computedSccp = PHSM.whatIsPercentOf(sccp, grossAmount);
 
@@ -206,7 +206,7 @@ let PHSM = (function () {
 	// return: int/float - computed sales tax
 	// ========================================================================
 	const computeSalesTax = function (grossAmount) {
-		const salesTax = charges.salesTax;
+		const salesTax = CHARGES.salesTax;
 		// get (sales tax)% of gross amount
 		let computedSalesTax = PHSM.whatIsPercentOf(salesTax, grossAmount);
 
@@ -287,13 +287,13 @@ let PHSM = (function () {
 		// ====================================================================
 		getBuyPreview: function (price, money) {
 			// we initialize every computation to assume the total cost
-			const boardLot = getBoardLot(price),
-				boardLotSize = boardLot.size,
-				minimumAmount = price * boardLotSize;
-			let canBuy = Math.floor(money / minimumAmount), // number of buyable boardlot
-				maxVolume = canBuy * boardLotSize,
-				grossAmount = PHSM.getGrossAmount(price, maxVolume),
-				totalCost = PHSM.getTotalBuyingCost(grossAmount);
+			const boardLot = getBoardLot(price);
+			const	boardLotSize = boardLot.size;
+			const	minimumAmount = price * boardLotSize;
+			let canBuy = Math.floor(money / minimumAmount); // number of buyable boardlot
+			let	maxVolume = canBuy * boardLotSize;
+			let	grossAmount = PHSM.getGrossAmount(price, maxVolume);
+			let	totalCost = PHSM.getTotalBuyingCost(grossAmount);
 
 			// since total cost must not exceed the current money
 			// we re-evaluate the values to include the charges
@@ -331,10 +331,10 @@ let PHSM = (function () {
 		// return: object - commission, vat, pseTransFee, sccp, totalFees
 		// ====================================================================
 		getBuyingFees: function (grossAmount) {
-			const commission = computeCommission(grossAmount),
-				vat = computeVat(commission),
-				pseTransFee = computePseTransFee(grossAmount),
-				sccp = computeSccp(grossAmount);
+			const commission = computeCommission(grossAmount);
+			const	vat = computeVat(commission);
+			const	pseTransFee = computePseTransFee(grossAmount);
+			const	sccp = computeSccp(grossAmount);
 			let totalFees = commission + vat + pseTransFee + sccp;
 
 			// round-off to 4 decimal and remove trailing zeros
@@ -354,11 +354,11 @@ let PHSM = (function () {
 		// return: object - commission, vat, pseTransFee, sccp, salesTax, totalFees
 		// ====================================================================
 		getSellingFees: function (grossAmount) {
-			const commission = computeCommission(grossAmount),
-				vat = computeVat(commission),
-				pseTransFee = computePseTransFee(grossAmount),
-				sccp = computeSccp(grossAmount),
-				salesTax = computeSalesTax(grossAmount);
+			const commission = computeCommission(grossAmount);
+			const	vat = computeVat(commission);
+			const	pseTransFee = computePseTransFee(grossAmount);
+			const	sccp = computeSccp(grossAmount);
+			const	salesTax = computeSalesTax(grossAmount);
 			let totalFees = commission + vat + pseTransFee + sccp + salesTax;
 
 			// round-off to 4 decimal and remove trailing zeros
@@ -386,16 +386,16 @@ let PHSM = (function () {
 			//if (arguments[0].constructor === Number) {
 			// if only one argument was passed, we'll use it as gross amount
 			if (arguments.length === 1) {
-				const grossAmount = arguments[0], // set the first argument as gross amount
-					buyingFees = PHSM.getBuyingFees(grossAmount);
+				const grossAmount = arguments[0]; // set the first argument as gross amount
+				const	buyingFees = PHSM.getBuyingFees(grossAmount);
 				totalBuyingCost = grossAmount + buyingFees.totalFees;
 			}
 			// if two arguments was passed, we'll use it as stock price and volume
 			else if (arguments.length === 2) {
-				const stockPrice = arguments[0], // set the first argument as stock price
-					volume = arguments[1], // set the second argument as volume
-					grossAmount = PHSM.getGrossAmount(stockPrice, volume),
-					buyingFees = PHSM.getBuyingFees(grossAmount);
+				const stockPrice = arguments[0]; // set the first argument as stock price
+				const volume = arguments[1]; // set the second argument as volume
+				const grossAmount = PHSM.getGrossAmount(stockPrice, volume);
+				const buyingFees = PHSM.getBuyingFees(grossAmount);
 				totalBuyingCost = grossAmount + buyingFees.totalFees;
 			}
 			//}
@@ -417,16 +417,16 @@ let PHSM = (function () {
 			//if (arguments[0].constructor === Number) {
 			// if only one argument was passed, we'll use it as gross amount
 			if (arguments.length === 1) {
-				const grossAmount = arguments[0], // set the first argument as gross amount
-					sellingFees = PHSM.getSellingFees(grossAmount);
+				const grossAmount = arguments[0]; // set the first argument as gross amount
+				const	sellingFees = PHSM.getSellingFees(grossAmount);
 				totalSellingNet = grossAmount - sellingFees.totalFees;
 			}
 			// if two arguments was passed, we'll use it as stock price and volume
 			else if (arguments.length === 2) {
-				const stockPrice = arguments[0], // set the first argument as stock price
-					volume = arguments[1], // set the second argument as volume
-					grossAmount = PHSM.getGrossAmount(stockPrice, volume),
-					sellingFees = PHSM.getSellingFees(grossAmount);
+				const stockPrice = arguments[0]; // set the first argument as stock price
+				const	volume = arguments[1]; // set the second argument as volume
+				const	grossAmount = PHSM.getGrossAmount(stockPrice, volume);
+				const	sellingFees = PHSM.getSellingFees(grossAmount);
 				totalSellingNet = grossAmount - sellingFees.totalFees;
 			}
 			//}
@@ -448,11 +448,11 @@ let PHSM = (function () {
 			// check if the passed argument is an Array,
 			// an Array means multiple transactions
 			if (arguments[0].constructor === Array) {
-				let transactions = arguments[0], // the arguments[0] is the actual array, and we set it to a variable
-					stockPrice,
-					volume,
-					totalCost = null,
-					totalVolume = null;
+				const transactions = arguments[0]; // the arguments[0] is the actual array, and we set it to a variable
+				let	stockPrice;
+				let	volume;
+				let	totalCost = null;
+				let	totalVolume = null;
 				// loop through the array to get individual transactions
 				for (let i = 0; i < transactions.length; i++) {
 					stockPrice = transactions[i].stockPrice, // set individual stock price
@@ -469,10 +469,10 @@ let PHSM = (function () {
 			// if the argument is not an Array,
 			// it means it's just a single transaction
 			else /*if (arguments[0].constructor === Number)*/ {
-				const stockPrice = arguments[0], // set the first argument as stock price
-					volume = arguments[1], // set the second argument as volume
-					totalCost = PHSM.getTotalBuyingCost(stockPrice, volume),
-					totalVolume = volume;
+				const stockPrice = arguments[0]; // set the first argument as stock price
+				const	volume = arguments[1]; // set the second argument as volume
+				const	totalCost = PHSM.getTotalBuyingCost(stockPrice, volume);
+				const	totalVolume = volume;
 				// compute for average
 				average = computeAverage(totalCost, totalVolume);
 			}
@@ -488,12 +488,13 @@ let PHSM = (function () {
 		// view here for more info on position sizing: http://www.chrisperruna.com/2007/09/18/reinforce-position-sizing/
 		// ====================================================================
 		getPositionSize: function (capital, riskPercent, stopLossPercent) {
-			let riskAmount, positionSize;
+			let riskAmount;
+			let positionSize;
 
 			riskAmount = PHSM.whatIsPercentOf(riskPercent, capital);
 			positionSize = (riskAmount / stopLossPercent) * 100;
 			positionSize = +positionSize.toFixed(4);
-			
+
 			return {
 				riskAmount,
 				positionSize
@@ -502,7 +503,7 @@ let PHSM = (function () {
 
 		// ====================================================================
 		// public method to compute Expectancy Rate per Trade
-		// params: int/float - winning rate, average win, lossing rate, average loss, 
+		// params: int/float - winning rate, average win, lossing rate, average loss,
 		//		   average risk amount
 		//         winning rate and lossing rate are percentage
 		//         while average win and average loss can be percentage or actual amount
@@ -550,8 +551,8 @@ let PHSM = (function () {
 
 		// ====================================================================
 		// public method to compute break-even price from original purchase
-		// params: int/float - 
-		// return: 
+		// params: int/float -
+		// return:
 		// ====================================================================
 		getBreakEvenPrice: function () {
 
@@ -564,15 +565,15 @@ let PHSM = (function () {
 		// ====================================================================
 		getHighLow: function (arr) {
 
-			const max = Math.max(...arr),
-				min = Math.min(...arr);
+			const max = Math.max(...arr);
+			const	min = Math.min(...arr);
 
 			return {
 				max: max,
 				min: min
 			};
 		},
-		
+
 		// ====================================================================
 		// public method to compute compunding
 		// params: int/float - amount, percent, cycle, additionPerCycle
@@ -591,10 +592,10 @@ let PHSM = (function () {
 					amount += interest + additionPerCycle;
 				}
 			}
-			
-			const percentIncrease = PHSM.getPercentDiff(totalInvestment, amount),
-				amountIncrease = amount - totalInvestment;
-			
+
+			const percentIncrease = PHSM.getPercentDiff(totalInvestment, amount);
+			const	amountIncrease = amount - totalInvestment;
+
 			return {
 				compoundedAmount: amount,
 				cycle: cycle,
